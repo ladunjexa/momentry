6a114c7a097e874c630856c825fa3f61f2c96606
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2025-07-30 04:54:18 +0300

    ...

--- src/app/[locale]/(showcase)/event/[eventId]/(site)/page.tsx
+++ src/app/[locale]/(showcase)/event/[eventId]/(site)/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
 
   return (
     <main>
-      <AuroraBackground>
+      <AuroraBackground className="z-100">
         <motion.div
           initial={{ opacity: 0.0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
--- src/app/[locale]/(showcase)/layout.tsx
+++ src/app/[locale]/(showcase)/layout.tsx
@@ -1,6 +1,47 @@
+"use client";
+
 import Footer from "@/components/marketing/layout/footer";
-import EventHero from "@/components/showcase/event-hero";
 import { Spacer } from "@heroui/react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { Icon as Iconify } from "@iconify/react";
+import { IconNewSection, IconTerminal2, IconHome } from "@tabler/icons-react";
+const links = [
+  {
+    title: "Momentry",
+    icon: (
+      <Iconify
+        icon="solar:home-linear"
+        className="h-full w-full text-foreground/50"
+      />
+    ),
+    href: "#",
+  },
+  {
+    title: "Momentry Showcase",
+    icon: (
+      <Iconify
+        icon="solar:gallery-linear"
+        className="h-full w-full text-foreground/50"
+      />
+    ),
+    href: "#",
+  },
+  {
+    title: "Momentry AI",
+    icon: <Iconify icon="ix:ai" className="h-full w-full text-foreground/50" />,
+    href: "#",
+  },
+  {
+    title: "Momentry Drop",
+    icon: (
+      <Iconify
+        icon="solar:box-linear"
+        className="h-full w-full text-foreground/50"
+      />
+    ),
+    href: "#",
+  },
+];
 
 export default function ShowcaseLayout({
   children,
@@ -11,8 +52,12 @@ export default function ShowcaseLayout({
       id="showcase-container"
     >
       <main className="w-full flex-grow">{children}</main>
-      <Spacer y={12} />
-      <Footer />
+
+      <div className="fixed bottom-4 left-0 right-0 z-50 flex justify-center">
+        <FloatingDock items={links} />
+      </div>
+      <Spacer y={24} />
+      {/* <Footer /> */}
     </div>
   );
 }
